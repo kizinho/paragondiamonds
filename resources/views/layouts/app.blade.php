f4fdf2ff54f304fd191a89fe6de4c08c1dbed29d
Kizinho <adikekizinho@gmail.com>
2022-11-23 00:29:39 +0100

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html  id="top" class="home-section home-page home-template" direction="ltr" env="" xmlns:og="//opengraphprotocol.org/schema/" lang="{{ str_replace('_', '-', app()->getLocale()) }}" />
 <head>
-<!--    <meta content="width=device-width, initial-scale=1.0" name="viewport" />-->
+<meta name="viewport" content="width=1024" />
       <meta name="HandheldFriendly" content="True" />
         <meta name="MobileOptimized" content="480" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
